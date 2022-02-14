cbba5536fc353319a31b6be41323c95572cef270
Ajax-thebug <65551283+Ajax-thebug@users.noreply.github.com>
2022-02-14 20:37:08 +0530

    Uploading Done

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+from plyer import notification
+ 
+ 
+if __name__=="__main__":
+
+    while True:    
+ 
+        notification.notify(
+            title = "BLINK EYE",
+            message= "Avoid Dryness In Your eye.",
+            app_icon= "D:\Projects\Eye Blink Notifier\eye.ico",
+           
+            # displaying time
+            timeout=2
+)
+        # waiting time
+        time.sleep(60)
