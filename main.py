79b49f2b8bec132241331230347eb91bbad7ff88
Ajax-thebug <65551283+Ajax-thebug@users.noreply.github.com>
2022-02-14 20:45:20 +0530

    title change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
     while True:    
  
         notification.notify(
-            title = "BLINK EYE",
+            title = "BLINK Please",
             message= "Avoid Dryness In Your eye.",
             app_icon= "D:\Projects\Eye Blink Notifier\eye.ico",
            
